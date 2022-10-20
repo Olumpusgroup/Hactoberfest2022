7d5df6aae2df942bb12fc9857fc99c2c4c8317dc
amanaolsingh <115914853+amanaolsingh@users.noreply.github.com>
2022-10-20 11:55:55 +0530

    Create LargestofTwo.java

---
+++ LargestofTwo.java
@@ -0,0 +1,27 @@
+public class LargestofTwo {
+	private static Scanner sc;
+	public static void main(String[] args) 
+	{
+		int number1, number2;
+		sc = new Scanner(System.in);
+		
+		System.out.print(" Please Enter the First Number : ");
+		number1 = sc.nextInt();	
+		
+		System.out.print(" Please Enter the Second Number : ");
+		number2 = sc.nextInt();	
+		
+		if(number1 > number2) 
+	    {
+			System.out.println("\n The Largest Number = " + number1);          
+	    } 
+	    else if (number2 > number1)
+	    { 
+	    	System.out.println("\n The Largest Number = " + number2);        
+	    } 
+	    else 
+	    {
+	    	System.out.println("\n Both are Equal");
+	    }		
+	}	
+}
